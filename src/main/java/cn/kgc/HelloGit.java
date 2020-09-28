42063a76536223040213e7954a2f10a66717ff1d
1849096983@qq.com <lexed@BD-02-27>
2020-09-28 16:09:53 +0800

    2

--- src/main/java/cn/kgc/HelloGit.java
+++ src/main/java/cn/kgc/HelloGit.java
@@ -5,6 +5,13 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("21345678");
+		System.out.println("98765");
+		System.out.println("3456789");
+		System.out.println("654567");
+		System.out.println("9876534567");
+		System.out.println("添加分支项目");
+		
 		
 	}
 
